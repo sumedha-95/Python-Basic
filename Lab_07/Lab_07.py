59236b1627056c3a96c6bc40f49fc73a6353c916
Sumedha Lakmal Bandaranayaka <it20643218@my.sliit.lk>
2022-05-14 19:28:21 +0530

    Bubble Sort and Selection Sort Algorithm

--- Lab_07/Lab_07.py
+++ Lab_07/Lab_07.py
@@ -35,3 +35,44 @@ def bubbleSort(A):
 bubbleSort(A)
 print("Sorted Array : ")
 print(A)
+
+
+
+#Question 2
+'''
+Consider the selection sort algorithm given below. Selection sort algorithm sorts n numbers stored in 
+array A by first finding the smallest element of A and exchanging it with the element in A[1]. Then 
+find the second smallest element of A, and exchange it with A[2]. Continue in this manner for the 
+first n - 1 elements of A.
+
+ SELECTION-SORT(A)
+1. n = A.length
+2. for j = 1 to n − 1
+3. smallest = j
+4. for i = j + 1 to n
+5. if A[i ] < A[smallest]
+6. smallest = i
+7. exchange A[ j ] with A[smallest]
+Write a program to sort a set of numbers using selection sort algorithm
+'''
+
+B = []
+for v in range(6):
+    B.append(int(input('Enter a Number :')))
+print(B)
+
+#implementation of selectionSort
+def selectionSort(B):
+    n = len(B)
+    for j in range (0,n-1):
+        smallest = j
+        for i in range (j+1,n):
+            if B[i] < B[smallest]:
+                smallest = i
+        B[j],B[smallest] = B[smallest],B[j]
+
+#call the selectionSort
+selectionSort(B)
+print("Sorted Array :")
+print(B)
+        
